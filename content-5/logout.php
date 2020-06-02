9e94b51773c0cbb624fca2a933143164553274a2
Felipe Contreras <felipe.contreras@example.com>
2020-06-02 12:38:32 -0400

    Upload Ofuscado

--- content-5/logout.php
+++ content-5/logout.php
@@ -1,11 +1,8 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
-if(isset($_COOKIE['User'])) {
-  unset($_COOKIE['User']);
-  setcookie('User', '', time() - 3600);
-}
-header("location:index.php");
-exit();
-?>
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.11  |
+    |              on 2020-06-02 13:28:11              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ goto jr63h; vQyV5: unset($_COOKIE["\125\x73\145\162"]); goto Qyj_v; OVQAa: session_destroy(); goto gjv6G; gjv6G: if (!isset($_COOKIE["\125\x73\x65\162"])) { goto fn8U0; } goto vQyV5; Mh9jh: header("\x6c\157\143\141\164\x69\x6f\156\72\151\156\x64\x65\x78\56\160\150\x70"); goto wKQiD; Qyj_v: setcookie("\x55\163\145\x72", '', time() - 3600); goto dKgUu; N61B9: session_unset(); goto OVQAa; jr63h: session_start(); goto N61B9; dKgUu: fn8U0: goto Mh9jh; wKQiD: exit;
--- includes/MySQLHandler.php
+++ includes/MySQLHandler.php
@@ -1,17 +1,8 @@
 <?php
-require_once(dirname(dirname(__FILE__)) . '/LocalSettings.php');
-
-ini_set('display_errors',1); 
- error_reporting(E_ALL);
-$con = mysql_connect($host,$dbuser,$dbpass);
-
-//mysql_select_db($dbname,$con);
-@mysql_select_db($dbname,$con) or die( "Unable to connect to the database: $dbname");
-
-
-if (!$con)
- {
- die('Could not connect: ' . mysql_error());
- }
-
-?>
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.11  |
+    |              on 2020-06-02 13:28:11              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ goto TekKb; VIWS0: $q9ll6 = WVFgO($ZfKzR, $pp1fS, $YsLmK); goto C55bY; TekKb: require_once dirname(dirname(__FILE__)) . "\57\114\157\x63\141\x6c\x53\x65\164\x74\x69\x6e\147\x73\x2e\x70\x68\x70"; goto q5Iz2; akrj7: die("\x43\x6f\165\154\x64\40\x6e\x6f\x74\x20\x63\157\156\156\x65\x63\164\72\40" . k9IMU()); goto oyxFe; C55bY: @sYFDW($FLVdc, $q9ll6) or die("\125\x6e\141\142\154\x65\x20\164\x6f\x20\143\x6f\x6e\156\145\143\164\40\164\157\x20\x74\150\145\x20\x64\141\x74\x61\x62\x61\x73\145\72\40{$FLVdc}"); goto bG5Em; bG5Em: if ($q9ll6) { goto T3lOU; } goto akrj7; zWE6_: error_reporting(E_ALL); goto VIWS0; q5Iz2: ini_set("\x64\x69\x73\160\154\x61\171\137\x65\162\162\x6f\x72\x73", 1); goto zWE6_; oyxFe: T3lOU:
